76dd2af0194ab6d2dc0c6607824c21a853b28670
Ruben De Smet <ruben.de.smet@rubdos.be>
2016-01-17 13:15:56 +0100

    Add dummytest so gradle wants to download the actual deps.

---
+++ android-java8-spiced/src/test/java/DummyTest.java
@@ -0,0 +1,17 @@
+import com.android.volley.VolleyError;
+import org.junit.Test;
+import org.robolectric.RobolectricGradleTestRunner;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+public class DummyTest {
+    public DummyTest() {
+    }
+    @Test
+    public void smokeTest() {
+        VolleyError e;
+        RobolectricGradleTestRunner rgtr;
+    }
+}
